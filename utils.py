7c4957406c98b26e127f3c5db8a0c8b911f0fbaf
Szymon Paja <szymusek45juve@gmail.com>
2026-06-09 17:55:06 +0200

    Utils updated

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+import time
+from pathlib import Path
+
+import pandas as pd
+
 AIRPORTS_ICAOS = {
     'JASIONKA': 'EPRZ',
     'BALICE': 'EPKK',
@@ -35,3 +40,33 @@ class Plane:
                     'on_ground': p.on_ground
                 } for p in self.track]
         }
+
+def extract_data():
+    start = time.time()
+    for i in range(12, 24):
+        print(f"{i} started...")
+        a = i if i > 9 else f"0{i}"
+        df = pd.read_csv(f"data/zipped/states_2017-06-05-{a}.csv.gz", compression="gzip")
+        df.to_parquet(f"data/processed/states_2017-06-05-{a}.parquet", index=False)
+        print(f"{i} finished")
+    end = time.time()
+    print(end - start)
+    return "Success!"
+
+COLS = ["time", "icao24", "lat", "velocity", "heading", "vertrate", "onground", "baroaltitude", "lon", "callsign"]
+def load_data() -> pd.DataFrame:
+    files = sorted(Path("data/processed").glob("states_2017-06-05-*.parquet"))
+    data = []
+    for f in files:
+        try:
+            d = pd.read_parquet(f, columns=COLS)
+            d.dropna(subset=["lat", "lon"])
+            data.append(d)
+            print(f"{f.name} OK")
+        except Exception as e:
+            print(f"{f.name} ERROR")
+            print(e)
+    #data = pd.concat([pd.read_parquet(f) for f in files], ignore_index=True)
+    print(data[0].columns)
+    return pd.concat(data[0:2], ignore_index=True)
+
